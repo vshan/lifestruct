e8a4058e1cba03199fa375ee3d39a4d05197b72e
Vinay Bhat <vinaybhat001@gmail.com>
2015-06-15 09:51:57 +0530

    playing around

--- lifestruct/app/controllers/homes_controller.rb
+++ lifestruct/app/controllers/homes_controller.rb
@@ -1,5 +1,16 @@
 class HomesController < ApplicationController
   def show
     @a = "Yo man wassup"
+    @dates = next_seven_days(Date.new(2015,6,30))
+  end
+
+  private
+
+  def next_seven_days(date)
+    dates = []
+    7.times do |num|
+      dates.push(date + num)
+    end
+    dates
   end
 end
