87359217f4cce06b3419202c70c8c9e6c8053288
Vinay Bhat <vinaybhat001@gmail.com>
2015-06-26 13:09:26 +0530

    display subgoals working

--- lifestruct/app/controllers/goals_controller.rb
+++ lifestruct/app/controllers/goals_controller.rb
@@ -2,4 +2,26 @@ class GoalsController < ApplicationController
   def index
     @goals = Goal.all
   end
+  
+  def create
+    @new_goal = Goal.new
+    title = goal_params[:title]
+    description = goal_params[:description]
+    deadline = goal_params[:deadline]
+    parent_id = goal_params[:parent_id].to_i
+    @new_goal.assign_attributes({title: title,
+       description: description,
+       deadline: deadline,
+       parent_id: parent_id
+      })
+    @new_goal.save
+    redirect_to '/home'
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:title, :description, :deadline, :parent_id)
+  end
+
 end
